b2a34cebca384e18eb5939729022390a7c86fca7
ckdudgus <c91878074@gmail.com>
2022-03-16 12:37:44 +0900

    nodereactserver

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express(); 
-
+var time = new Date();
 
 var postsend = require('./api/postsend')
 var getsend = require('./api/getsend')
@@ -15,7 +15,7 @@ app.use('/postsend' , postsend)
 app.use('/getsend' , getsend)
 
 app.listen(app.get('port'), () => {
-    console.log('콘솔확인')
+    console.log(time +'콘솔확인')
 })
 
 
