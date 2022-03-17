adb7344c09960aa821ef4c122f7c27f0f5aec110
ckdudgus <c91878074@gmail.com>
2022-03-17 18:24:28 +0900

    commit

---
+++ front/src/component/Get.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+
+export default function Get(){
+    const [origintext , originUpdate] = useState('내가 먼저');
+
+    useEffect( async () => {
+        const getdata = await axios.get('/getsend')
+        originUpdate(getdata)
+    } 
+    , [] );
+    
+  return (
+    <div>{origintext}</div>
+  )
+}
