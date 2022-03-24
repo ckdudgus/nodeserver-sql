901780027c2e15cd1ac4bf1217fc564a93b27e10
ckdudgus <c91878074@gmail.com>
2022-03-24 15:04:41 +0900

    node

--- front/src/component/Interview.js
+++ front/src/component/Interview.js
@@ -1,31 +1,37 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState} from 'react'; 
 import axios from 'axios';
-
-const Interview = (props) => {
-    const [interviewId , interviewUpdate] = useState(0);
-    const interviewPublic = async () => {
-                            await axios.get(`/cyhpreinterview?botable=${props.botable}`)
-                                       .then(
-                                           (result) => {
-                                               console.log(result.data)
-                                           }
+const Interview = (props) => {   
+    let [ interviewId, interviewIdUpdate ] = useState([]);  
+    const [typeData,insertDB] = useState(0);   
+   
+    const interviewDataSetting = async () => {                      
+                             await axios.get(`/cyhpreinterview?botable=${props.botable}`)
+                                        .then(
+                                            (result) => {  
+                                                try{  
+                                                    interviewIdUpdate([...result.data]); 
+                                                    insertDB(result.data[0].key_id);                                                 
+                                                }
+                                                catch(err){ console.log(err.message) }
+                                            }
                                         )
-                                       .catch ( e => {
-                                           console.log(e +'이유로 통신이 불안전함')
-                                           }
-                                        )
-    }
-
-    useEffect( () => {
-        interviewPublic();
-    } , [] )
-
-        return (
-            <div>
-                콘솔창을 확인해봐
+                                        .catch ( e => { console.log(e +'이유로 통신이 불안전함') }
+                                        ) 
+    } 
+    useEffect( () => {  interviewDataSetting(); } , [typeData]  )          
+        return (  
+            <div> <h2>{ interviewId.length > 0 ? "사전인터뷰" : "데이터전송중..." }</h2>
+             {
+                 interviewId.map(( contant, i ) => {
+                     return(
+                         <li>
+                             <h3>{i+1} {contant.cyh_subject}</h3><div>{contant.cyh_content}</div>
+                         </li>
+                     )
+                 })
+             }
             </div>
-        );
+        );   
 }
-
 export default Interview;
  
